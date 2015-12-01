d9b9d441c370faa79816f4835c6276ed28c6d832
Nicolas Berger <nicoberger@gmail.com>
2015-12-01 16:59:53 -0300

    Also run lean-map.test.core tests with doo

--- test/cljs/lean_map/runner.cljs
+++ test/cljs/lean_map/runner.cljs
@@ -1,5 +1,7 @@
 (ns cljs.lean-map.runner
     (:require [doo.runner :refer-macros [doo-tests]]
-              [cljs.lean-map.test.collection-check]))
+              [cljs.lean-map.test.collection-check]
+              [cljs.lean-map.test.core]))
 
-(doo-tests 'cljs.lean-map.test.collection-check)
+(doo-tests 'cljs.lean-map.test.collection-check
+           'cljs.lean-map.test.core)
